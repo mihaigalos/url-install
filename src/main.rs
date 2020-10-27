326c46f523913a717a3feb06cef11eaf18ac0068
Mihai Galos <mihaigalos@gmail.com>
2020-10-27 17:29:52 +0100

    Working download PoC

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
-extern crate reqwest;
-
+use error_chain::error_chain;
 use std::fs::File;
-use std::io;
+use std::io::prelude::*;
+use std::path::Path;
 
-fn main() {
-    let mut resp = reqwest::get("https://sh.rustup.rs").expect("request failed");
-    let mut out = File::create("rustup-init.sh").expect("failed to create file");
-    io::copy(&mut resp, &mut out).expect("failed to copy content");
+error_chain! {
+     foreign_links {
+         Io(std::io::Error);
+         HttpRequest(reqwest::Error);
+     }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let target = "https://github.com/twbs/bootstrap/archive/v4.0.0.zip";
+    let response = reqwest::get(target).await?;
+    assert!(response.status().is_success());
+
+    let content = response.bytes().await?;
+
+    let path = Path::new("./download.zip");
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}", why),
+        Ok(file) => file,
+    };
+    file.write_all(&content)?;
+    Ok(())
 }
