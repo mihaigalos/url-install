dfa18b1697b87fb876248d1455436c5c93d89c91
Mihai Galos <mihaigalos@gmail.com>
2020-10-27 20:05:12 +0100

    Speculative separate implementation, got 403 forbidden

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,88 @@
-use error_chain::error_chain;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+use std::{
+    fs,
+    io::{self, copy, Read},
+    path::Path,
+};
 
-error_chain! {
-     foreign_links {
-         Io(std::io::Error);
-         HttpRequest(reqwest::Error);
-     }
+use exitfailure::ExitFailure;
+use indicatif::{ProgressBar, ProgressStyle};
+use reqwest::Url;
+use reqwest::{header, Client};
+
+struct DownloadProgress<R> {
+    inner: R,
+    progress_bar: ProgressBar,
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let target = "https://github.com/Byron/dua-cli/releases/download/v2.10.2/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz";
-    let response = reqwest::get(target).await?;
-    assert!(response.status().is_success());
+impl<R: Read> Read for DownloadProgress<R> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.inner.read(buf).map(|n| {
+            self.progress_bar.inc(n as u64);
+            n
+        })
+    }
+}
 
-    let content = response.bytes().await?;
+fn download(url: &str) -> Result<(), ExitFailure> {
+    let url = Url::parse(url)?;
+    let client = Client::new();
 
-    let path = Path::new("dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz");
-
-    let mut file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}", why),
-        Ok(file) => file,
+    let total_size = {
+        let resp = client.head(url.as_str()).send()?;
+        println!("{}", resp.status());
+        if resp.status().is_success() {
+            resp.headers()
+                .get(header::CONTENT_LENGTH)
+                .and_then(|ct_len| ct_len.to_str().ok())
+                .and_then(|ct_len| ct_len.parse().ok())
+                .unwrap_or(0)
+        } else {
+            return Err(failure::err_msg(format!(
+                "Couldn't download URL: {}. Error: {:?}",
+                url,
+                resp.status(),
+            ))
+            .into());
+        }
     };
-    file.write_all(&content)?;
+
+    let mut request = client.get(url.as_str());
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+                 .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+                 .progress_chars("#>-"));
+
+    let file = Path::new(
+        url.path_segments()
+            .and_then(|segments| segments.last())
+            .unwrap_or("tmp.bin"),
+    );
+
+    if file.exists() {
+        let size = file.metadata()?.len() - 1;
+        request = request.header(header::RANGE, format!("bytes={}-", size));
+        pb.inc(size);
+    }
+
+    let mut source = DownloadProgress {
+        progress_bar: pb,
+        inner: request.send()?,
+    };
+
+    let mut dest = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&file)?;
+
+    let _ = copy(&mut source, &mut dest)?;
+
+    println!(
+        "Download of '{}' has been completed.",
+        file.to_str().unwrap()
+    );
+
     Ok(())
+}
+fn main() {
+    download("https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip");
 }
