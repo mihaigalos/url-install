1ed7fb391a6240782aca60027b2c7410bee54780
Mihai Galos <mihaigalos@gmail.com>
2020-10-27 21:45:40 +0100

    Read archive from commandline

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
+use clap::App;
+
 error_chain! {
      foreign_links {
          Io(std::io::Error);
@@ -10,9 +12,26 @@ error_chain! {
      }
 }
 
+fn target_with_extension(s: &str) -> &str {
+    let pos_of_last_slash = s.rfind('/').unwrap();
+    &s[pos_of_last_slash + 1..]
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let target = "https://github.com/Byron/dua-cli/releases/download/v2.10.2/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz";
+    let matches = App::new("url-install")
+        .arg("<output> 'Sets an optional output file'")
+        .get_matches();
+
+    let mut target = ""; // https://github.com/Byron/dua-cli/releases/download/v2.10.2/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz
+
+    if let Some(o) = matches.value_of("output") {
+        target = o;
+    }
+
+    let _name_with_extension = target_with_extension(&target).to_string();
+    println!("{}", _name_with_extension);
+
     let response = reqwest::get(target).await?;
     assert!(response.status().is_success());
 
