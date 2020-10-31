f19398e5af0b2b9df64ab1495c0f32736dc017fe
Mihai Galos <mihaigalos@gmail.com>
2020-10-31 09:11:36 +0100

    Test zip decompression

--- src/decompressor.rs
+++ src/decompressor.rs
@@ -4,6 +4,8 @@ use std::fs::File;
 use std::path::Path;
 use tar::Archive;
 
+use unzip::Unzipper;
+
 pub struct TarGzDecompressor;
 impl Decompressor for TarGzDecompressor {
     fn run(&self, file: &str) -> std::io::Result<()> {
@@ -15,21 +17,38 @@ impl Decompressor for TarGzDecompressor {
     }
 }
 
+pub struct ZipDecompressor;
+impl Decompressor for ZipDecompressor {
+    fn run(&self, file: &str) -> std::io::Result<()> {
+        Unzipper::new(File::open(file).unwrap(), ".")
+            .unzip()
+            .unwrap();
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn targz_decompression_works() {
-        let mut is_file_present = false;
         let in_file = "test/test_decompression.tar.gz";
-        let out_file = "test_decompression.txt";
+        let out_file = "test_decompression.tar.gz.txt";
 
         TarGzDecompressor {}.run(in_file).unwrap();
-        if Path::new(out_file).exists() {
-            is_file_present = true;
-        }
+        assert!(Path::new(out_file).exists());
         std::fs::remove_file(out_file).unwrap();
-        assert!(is_file_present);
+    }
+
+    #[test]
+    fn zip_decompression_works() {
+        let in_file = "test/test_decompression.zip";
+        let out_file = "test_decompression.zip.txt";
+
+        ZipDecompressor {}.run(in_file).unwrap();
+        assert!(Path::new(out_file).exists());
+        std::fs::remove_file(out_file).unwrap();
     }
 }
