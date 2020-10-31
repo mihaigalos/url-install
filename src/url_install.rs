ee325c915b0c4a8df7ca7c4bef15dfbc49dab97b
Mihai Galos <mihaigalos@gmail.com>
2020-10-31 10:13:11 +0100

    Give executable permissions to extracted file

--- src/url_install.rs
+++ src/url_install.rs
@@ -1,6 +1,11 @@
 use crate::slicer::Slicer;
 use crate::traits::{Decompressor, Downloader};
 
+#[cfg(target_os = "linux")]
+use std::os::unix::fs::PermissionsExt;
+
+use std::fs;
+
 pub struct UrlInstall {
     pub downloader: Box<dyn Downloader>,
     pub decompressor: Box<dyn Decompressor>,
@@ -10,6 +15,20 @@ impl UrlInstall {
         let file = Slicer::target_with_extension(from_url);
         self.downloader.get(from_url, file)?;
         self.decompressor.run(file)?;
+
+        self.ensure_executable_permissions(file)?;
+
+        Ok(())
+    }
+
+    #[cfg(any(target_os = "linux", target_os = "mac"))]
+    fn ensure_executable_permissions(&self, file: &str) -> std::io::Result<()> {
+        fs::set_permissions(file, fs::Permissions::from_mode(0o755))?;
+        Ok(())
+    }
+
+    #[cfg(target_os = "windows")]
+    fn ensure_executable_permissions(&self, file: &str) -> std::io::Result<()> {
         Ok(())
     }
 }
