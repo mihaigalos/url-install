77f9dbb9b232e2e7a07701270ecef5febf155a5b
Mihai Galos <mihaigalos@gmail.com>
2020-10-31 08:20:21 +0100

    Fix namespace reference to std::fs::remove_file

--- src/decompressor.rs
+++ src/decompressor.rs
@@ -13,3 +13,20 @@ impl Decompressor for TarGzDecompressor {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn targz_decompression_works() {
+        let mut is_file_downloaded = false;
+        let file = "dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz";
+        BlockingDownloader{}.get("https://github.com/Byron/dua-cli/releases/download/v2.10.2/dua-v2.10.2-x86_64-unknown-linux-musl.tar.gz", file).unwrap();
+        if Path::new(file).exists() {
+            is_file_downloaded = true;
+        }
+        fs::remove_file(file).unwrap();
+        assert!(is_file_downloaded);
+    }
+}
